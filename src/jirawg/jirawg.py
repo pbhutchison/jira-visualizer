7f5d3aa8430bca722d3883b54b5013b6fa806dde
Paul Hutchison <pbhutchison04@gmail.com>
2024-05-06 16:58:38 -0400

    Set up somd nicegui structure based off of website

---
+++ src/jirawg/jirawg.py
@@ -0,0 +1,14 @@
+from nicegui import ui
+def main():
+    ui.icon('thumb_up')
+    ui.markdown('This is **Markdown**.')
+    ui.html('This is <strong>HTML</strong>.')
+    with ui.row():
+        ui.label('CSS').style('color: #888; font-weight: bold')
+        ui.label('Tailwind').classes('font-serif')
+        ui.label('Quasar').classes('q-ml-xl')
+    ui.run(port=6060)
+
+
+if __name__ in ("__main__", "__mp_main__"):
+    main()
