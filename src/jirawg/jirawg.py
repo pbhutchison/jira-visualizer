e279dacad4da14a173faa95c9879d1bc6f61ce19
Paul Hutchison <pbhutchison04@gmail.com>
2024-05-07 13:42:55 -0400

    Created stylesheet, set up automatic linting, included stylesheet on main method.

--- src/jirawg/jirawg.py
+++ src/jirawg/jirawg.py
@@ -1,16 +1,14 @@
 from nicegui import ui
+
 def main():
-    ui.icon('thumb_up')
-    ui.markdown('This is **Markdown**.')
-    ui.html('This is <strong>HTML</strong>.')
-    with ui.row():
-        ui.label('CSS').style('color: #888; font-weight: bold')
-        ui.label('Tailwind').classes('font-serif')
-        ui.label('Quasar').classes('q-ml-xl')
-    dark = ui.dark_mode()
-    dark.enable()
-    ui.run(port=6060)
+  ui.add_head_html('<link rel="stylesheet" type="text/css" href="/styles/style.css">')
+  with ui.row():
+    header = ui.label('Jira Web Visualizer')
+  # with ui.row():
+  #     ui.text_box()
+  dark = ui.dark_mode()
+  dark.enable()
+  ui.run(port=6060)
 
-
-if __name__ in ("__main__", "__mp_main__"):
-    main()
+if __name__ in ('__main__', '__mp_main__'):
+  main()
