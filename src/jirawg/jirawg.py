3d796a68043e23e1ccaaf8340faeb6f72ece8ff2
Paul Hutchison <pbhutchison04@gmail.com>
2024-05-07 20:39:33 -0400

    Fixed styling of existing elements.

--- src/jirawg/jirawg.py
+++ src/jirawg/jirawg.py
@@ -1,11 +1,11 @@
 from nicegui import ui
 
 def main():
-  ui.add_head_html('<link rel="stylesheet" type="text/css" href="/styles/style.css">')
-  with ui.row():
-    header = ui.label('Jira Web Visualizer')
-  # with ui.row():
-  #     ui.text_box()
+  with ui.row().style('width: 100%;'):
+    header_styles = 'color: #00b4ff; '
+    header_styles = header_styles.join('margin: auto; ')
+    header_styles = header_styles.join('font-size: 300%; ')
+    ui.label('Jira Web Visualizer').style(header_styles)
   dark = ui.dark_mode()
   dark.enable()
   ui.run(port=6060)
